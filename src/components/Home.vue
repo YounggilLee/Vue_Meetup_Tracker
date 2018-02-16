4925e44767f4f13229a5e3f66a6aff03dae7f2a9
GiL <iamleeyounggil@gmail.com>
2018-02-16 12:28:54 -0500

    building home page using carousel item(vuetify)

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,55 @@
 <template>
-  <div>
-    <p> The Home Page</p>
-  </div>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-right">
+        <v-btn large router to="/meetups" class="info">Explore Meetups</v-btn>
+      </v-flex>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-left">
+        <v-btn large router to="/meetup/new" class="info">Organize Meetup</v-btn>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap class="mt-2">
+      <v-flex xs12>
+        <v-carousel>
+          <v-carousel-item
+            v-for="meetup in meetups"
+            :src="meetup.imageUrl"
+            :key="meetup.id">
+            <div class="title">
+              {{ meetup.title }}
+            </div>
+          </v-carousel-item>
+        </v-carousel>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap  class="mt-2">
+      <v-flex xs12 class="text-xs-center">
+       <p>Join our awesome meetups!</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
+<script>
+  export default {
+    data () {
+      return {
+        meetups: [
+          { imageUrl: 'https://www.toronto.ca/wp-content/uploads/2017/07/9163-invest-in-toronto-995x330.png', id: 'abc', title: 'Meetup in Toronto' },
+          { imageUrl: 'https://www.travelgayasia.com/wp-content/uploads/2012/06/Soeul-City-Guide.jpg', id: 'def', title: 'Meetup in Seoul' }
+        ]
+      }
+    }
+  }
+</script>
 
+<style scoped>
+  .title {
+    position: absolute;
+    bottom: 50px;
+    background-color: rgba(0,0,0,0.5);
+    color: white;
+    font-size: 2em;
+    padding: 20px;
+  }
+</style>
