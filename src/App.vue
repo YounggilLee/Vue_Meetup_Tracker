c0ce30e5f5169f467ee1840efed6e23756900317
GiL <iamleeyounggil@gmail.com>
2018-02-16 12:06:18 -0500

    Implement router in menu and header

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,31 @@
 <template>
   <v-app>
-
       <v-toolbar dark class="black">
         <v-toolbar-side-icon @click="sideNav = !sideNav"
        ></v-toolbar-side-icon>
-        <v-toolbar-title>MeetUp Tracker</v-toolbar-title>
+        <v-toolbar-title>
+          <router-link
+          to="/"
+          tag="span"
+          style="cursor:pointer">
+          MeetUp Tracker
+          </router-link>
+          </v-toolbar-title>
         <v-spacer></v-spacer>
         <v-toolbar-items>
-          <v-btn flat >
-            <v-icon dark left>supervisor_account</v-icon>
-            View Meetups
+          <v-btn flat v-for="item in menuItems" :key="item.title"
+                router
+                :to="item.link">
+            <v-icon dark left>{{item.icon}}</v-icon>
+           {{item.title}}
             </v-btn>
         </v-toolbar-items>
       </v-toolbar>
-      <v-navigation-drawer v-model="sideNav">
+      <v-navigation-drawer temporary v-model="sideNav">
         <v-list>
-          <v-list-tile v-for="item in menuItems" :key="item.title">
+          <v-list-tile v-for="item in menuItems" :key="item.title"
+                  router
+                  :to="item.link">
           <v-list-tile-action>
             <v-icon>{{item.icon}}</v-icon>
           </v-list-tile-action>
@@ -35,11 +45,11 @@ export default {
     return {
       sideNav: false,
       menuItems: [
-        {icon: 'supervisor_account', title: 'View Meetups'},
-        {icon: 'room', title: 'Organize Meetip'},
-        {icon: 'person', title: 'Profile'},
-        {icon: 'face', title: 'Sign up'},
-        {icon: 'lock_open', title: 'Sign in'}
+        { icon: 'supervisor_account', title: 'View Meetups', link: '/meetups' },
+          { icon: 'room', title: 'Organize Meetup', link: '/meetup/new' },
+          { icon: 'person', title: 'Profile', link: '/profile' },
+          { icon: 'face', title: 'Sign up', link: '/signup' },
+          { icon: 'lock_open', title: 'Sign in', link: '/signin' }
       ]
   }
 
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,12 +4,4 @@
   </div>
 </template>
 
-<script>
-export default {
 
-}
-</script>
-
-<style>
-
-</style>
